7115297ceca9cfafea892d80e8fa37615b92a0b5
KateBlazhko <kateugnicheva@gmail.com>
2022-12-01 19:20:14 +0300

    feat: implement cli block

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2)
+    args.forEach((arg, index, arr) => {
+        if (index === arr.length - 1) {
+            process.stdout.write(`${arg}\n`)
+            return 
+        }
+
+        if (index % 2 === 0) {
+            process.stdout.write(`${arg.slice(2)} is `)
+        } else {
+            process.stdout.write(`${arg}, `)
+        }
+    })
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+    const allEnvs = process.env
+    for (const oneEnv in allEnvs) {
+        if (oneEnv.startsWith('RSS_')) {
+            process.stdout.write(`${oneEnv}=${allEnvs[oneEnv]}; `)
+        }
+    }
 };
 
 parseEnv();
