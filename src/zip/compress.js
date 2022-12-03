94f71afbd6144a2ed6cd4d0147efc852221276e9
KateBlazhko <kateugnicheva@gmail.com>
2022-12-03 15:24:38 +0300

    feat: implement zip block

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import { createGzip } from 'zlib'
+import {pipeline} from 'stream'
+import {createReadStream,createWriteStream } from 'fs';
+import { join } from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const compress = async () => {
-    // Write your code here 
+    const pathToFileSrc = join(__dirname, 'files/fileToCompress.txt')
+    const pathToFileDest = join(__dirname, 'files/archive.gz')
+
+    const gzip = createGzip();
+    const source = createReadStream(pathToFileSrc);
+    const destination = createWriteStream(pathToFileDest);
+
+    pipeline(source, gzip, destination, (err) => err && console.error(err.message));
 };
 
 await compress();
