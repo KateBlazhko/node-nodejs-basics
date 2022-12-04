634ec9e45092271f519d160fa003994056fcf693
KateBlazhko <kateugnicheva@gmail.com>
2022-12-04 15:35:17 +0300

    feat: implement worker block

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,39 @@
+import { Worker } from 'worker_threads'
+import { join } from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const INITIAL_NUMBER = 10
+const LOGICAL_CORES = 12
+
+const runWorker =(workerData) => {
+    const pathToWorker = join(__dirname, 'worker.js')
+
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(pathToWorker, { workerData });
+        worker.on('message', resolve);
+        worker.on('error', reject);
+        worker.on('exit', (code) => {
+          if (code !== 0)
+            reject(new Error(`Worker stopped with exit code ${code}`));
+        })
+    })
+}
+
 const performCalculations = async () => {
-    // Write your code here
+    const resultArr = []
+    for (let i = 0; i < LOGICAL_CORES; i++) {
+        try {
+            const result = await runWorker(INITIAL_NUMBER + i)
+            resultArr.push({status: 'resolved', data: result.result})
+        }
+        catch (error) {
+            resultArr.push({status: 'error', data: null})
+    
+        }
+    }
+    console.log(resultArr)
+
 };
 
 await performCalculations();
