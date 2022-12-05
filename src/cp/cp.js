4fcf8f074709bf1edd7fb0420fb48db2861e5e9d
KateBlazhko <kateugnicheva@gmail.com>
2022-12-05 18:58:42 +0300

    feat: implement cp block

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,13 @@
+import { fork } from 'child_process';
+import { join } from 'path';
+import * as url from 'url';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const pathToChild = join(__dirname, 'files/script.js')
+
+    fork(pathToChild, args)
 };
 
 spawnChildProcess();
